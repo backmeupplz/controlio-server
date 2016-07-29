a30e4daedf5977b36fa52fa6324b6a7c3655f6ee
backmeupplz <backmeupplz@gmail.com>
2016-07-28 19:59:48 -0700

    remove base route

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ const auth = require('./helpers/auth');
 const users = require('./routes/users');
 const projects = require('./routes/projects');
 const posts = require('./routes/posts');
-const base = require('./routes/base');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -44,7 +43,6 @@ app.use(auth.checkApiKey);
 app.use('/users/', users);
 app.use('/projects', projects);
 app.use('/posts/', posts);
-app.use('/', base);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/base.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.sendStatus(200);
-});
-
-module.exports = router;
