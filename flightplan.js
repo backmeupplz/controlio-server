1aab773b697a9af708b40f18b352554b4fc840fa
backmeupplz <backmeupplz@gmail.com>
2016-07-28 19:58:09 -0700

    refactor filghtplan.js

--- flightplan.js
+++ flightplan.js
@@ -24,19 +24,19 @@ plan.target('production', [
 
 plan.local(function(local) {
   local.log('Copy files to remote hosts');
-  var filesToCopy = local.exec('git ls-files', {silent: true});
+  const filesToCopy = local.exec('git ls-files', { silent: true });
   local.transfer(filesToCopy, `/tmp/${ tmpDir }`);
 });
 
 plan.remote(function(remote) {
   remote.log('Move folder to root');
-  remote.sudo(`cp -R /tmp/${ tmpDir } ~`, {user: username});
+  remote.sudo(`cp -R /tmp/${ tmpDir } ~`, { user: username });
   remote.rm(`-rf /tmp/${ tmpDir }`);
 
   remote.log('Install dependencies');
-  remote.sudo(`npm --production --prefix ~/${ tmpDir } install ~/${ tmpDir }`, {user: username});
+  remote.sudo(`npm --production --prefix ~/${ tmpDir } install ~/${ tmpDir }`, { user: username });
 
   remote.log('Reload application');
-  remote.sudo(`ln -snf ~/${ tmpDir } ~/${ appName }`, {user: username});
+  remote.sudo(`ln -snf ~/${ tmpDir } ~/${ appName }`, { user: username });
   remote.exec('sudo restart controlio');
 });
