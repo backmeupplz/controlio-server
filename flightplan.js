25d245ad28c78fc3d726c714ba7dcf183d2c73a6
backmeupplz <backmeupplz@gmail.com>
2016-10-04 22:50:06 -0700

    fix flightplan

--- flightplan.js
+++ flightplan.js
@@ -3,40 +3,40 @@ const plan = require('flightplan');
 const appName = 'controlio';
 const username = 'deploy';
 
-const tmpDir = `${ appName }-${ new Date().getTime() }`;
+const tmpDir = `${appName}-${new Date().getTime()}`;
 
 // configuration
 plan.target('staging', [
   {
-    host: '107.170.84.246',
-    username: username,
-    agent: process.env.SSH_AUTH_SOCK
-  }
+    host: '162.243.76.239',
+    username,
+    agent: process.env.SSH_AUTH_SOCK,
+  },
 ]);
 
 plan.target('production', [
   {
-    host: '107.170.84.246',
-    username: username,
-    agent: process.env.SSH_AUTH_SOCK
-  }
+    host: '162.243.76.239',
+    username,
+    agent: process.env.SSH_AUTH_SOCK,
+  },
 ]);
 
-plan.local(function(local) {
+plan.local((local) => {
   local.log('Copy files to remote hosts');
   const filesToCopy = local.exec('git ls-files', { silent: true });
-  local.transfer(filesToCopy, `/tmp/${ tmpDir }`);
+  local.transfer(filesToCopy, `/tmp/${tmpDir}`);
 });
 
-plan.remote(function(remote) {
+plan.remote((remote) => {
   remote.log('Move folder to root');
-  remote.sudo(`cp -R /tmp/${ tmpDir } ~`, { user: username });
-  remote.rm(`-rf /tmp/${ tmpDir }`);
+  remote.sudo(`cp -R /tmp/${tmpDir} ~`, { user: username });
+  remote.rm(`-rf /tmp/${tmpDir}`);
 
   remote.log('Install dependencies');
-  remote.sudo(`npm --production --prefix ~/${ tmpDir } install ~/${ tmpDir }`, { user: username });
+  remote.sudo(`npm --production --prefix ~/${tmpDir} install ~/${tmpDir}`, { user: username });
 
   remote.log('Reload application');
-  remote.sudo(`ln -snf ~/${ tmpDir } ~/${ appName }`, { user: username });
+  remote.sudo(`ln -snf ~/${tmpDir} ~/${appName}`, { user: username });
   remote.exec('sudo restart controlio');
 });
