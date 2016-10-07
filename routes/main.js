241b63cb443b619dbe9ca412f7ee0c6608cf7ecc
backmeupplz <backmeupplz@gmail.com>
2016-10-06 18:30:33 -0700

    fix apple route for system file

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/.well-known/apple-app-site-association', (req, res) => {
+  res.send({
+    applinks: {
+      apps: [],
+      details: [
+        {
+          appID: '9VUB6L23QH.BorodutchStudio.Controlio',
+          paths: ['/magic'],
+        },
+      ],
+    },
+  });
+});
+
 router.get('/apple-app-site-association', (req, res) => {
   res.send({
     applinks: {
