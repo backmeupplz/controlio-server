2e14a6d3d1fec751e6b90accc858d85768f8e742
backmeupplz <backmeupplz@gmail.com>
2016-10-21 16:15:42 -0700

    change config variables to env vars

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  apiKey: 'p[oqkix=%FAi]&FMAYnLUJJWC,w',
-  jwtSecret: 'icf&*RoAC{|8,#t7;|3;kA=f7knOY/',
-  database: 'mongodb://admin:5xohSDtRLr4HkZA1ncXk2CiGQXJ26z@162.243.42.134:27017/controlio',
-  sendgridApiKey: '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',
-  randomTokenSalt: '6GuTvW1X1lPyt90vWmMxu24tFz39r0A1',
-  stripeApiKey: 'sk_live_An35Qqmfvufq59lmqLvzfN9g',
+  apiKey: process.env.API_KEY,
+  jwtSecret: process.env.JWT_SECRET,
+  database: process.env.MONGO_URL,
+  sendgridApiKey: process.env.SENDGRID_API_KEY,
+  randomTokenSalt: process.env.TOKEN_SALT,
+  stripeApiKey: process.env.STRIPE_API_KEY,
 };
