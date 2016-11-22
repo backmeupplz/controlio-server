871734f41ebb558d475d6cf7aeb670633587ee90
backmeupplz <backmeupplz@gmail.com>
2016-11-21 22:55:06 -0800

    add banned urls

--- helpers/botReporter.js
+++ helpers/botReporter.js
@@ -7,13 +7,12 @@ const jarvis = new TelegramBot(config.telegramKey, { polling: false });
 // General
 
 function reportError(err, req) {
-  if (String(req.get('host')) === 'forum.khoapham.vn') { return; }
-  const bannedUrlIncludes = ['nikita', 'n1kita'];
-  for (let i in bannedUrlIncludes) {
-    if (String(req.originalUrl).toLowerCase().indexOf(bannedUrlIncludes[i]) > -1) {
+  const bannedUrlIncludes = ['nikita', 'n1kita', 'forum.khoapham.vn', '162.243.76.239', 'maroonpaymentsystems.com', 'maroonpay.com', 'healthepaymentservices.com'];
+  bannedUrlIncludes.forEach((bannedUrl) => {
+    if (String(req.originalUrl).toLowerCase().indexOf(bannedUrl) > -1) {
       return;
     }
-  }
+  });
   const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
   sendMessage(`❗ *Error!*\n${'```'}json\n${fullUrl} – ${req.method}\nheaders: ${JSON.stringify(req.headers, null, 2)}\nbody: ${JSON.stringify(req.body, null, 2)}\n${JSON.stringify(err, null, 2)}${'```'}`);
 }
