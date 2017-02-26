cbbc1154ba69fa361472b4ce19448da67dcfe7a3
backmeupplz <backmeupplz@gmail.com>
2017-02-25 23:25:46 -0800

    add docs to auth.js

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,3 +1,11 @@
+/**
+ * Middleware for api authorization
+ *
+ * @module app
+ * @license MIT
+ */
+
+/** Dependencies */
 const errors = require('./errors');
 const dbmanager = require('./dbmanager');
 const jwt = require('jsonwebtoken');
@@ -5,21 +13,22 @@ const config = require('../config');
 const validate = require('express-validation');
 const validation = require('../validation/auth');
 
+/** Method to check if request is signed with a valid user token */
 function checkToken(req, res, next) {
-  validate(validation.token)(req, res, err => {
+  validate(validation.token)(req, res, (err) => {
     if (err) return next(err);
   });
 
   const token = req.get('token');
   const userId = req.get('userId');
 
-  jwt.verify(token, config.jwtSecret, err => {
+  jwt.verify(token, config.jwtSecret, (err) => {
     if (err) {
       return next(err);
     }
     dbmanager.findUserById(userId)
       .select('token')
-      .then(user => {
+      .then((user) => {
         if (!user) {
           return next(errors.authEmailNotRegistered());
         } else if (user.token !== token) {
@@ -27,23 +36,25 @@ function checkToken(req, res, next) {
         }
         next();
       })
-      .catch(err => next(err));
+      .catch(error => next(error));
   });
-};
+}
 
+/** Method to check if request is signed with a valid api key */
 function checkApiKey(req, res, next) {
-  validate(validation.apiKey)(req, res, err => {
+  validate(validation.apiKey)(req, res, (err) => {
     if (err) return next(err);
   });
   const apiKey = req.get('apiKey');
-  if (apiKey == config.apiKey) {
+  if (apiKey === config.apiKey) {
     next();
   } else {
     next(errors.noApiKey());
   }
-};
+}
 
+/** Exports */
 module.exports = {
   checkToken,
-  checkApiKey
+  checkApiKey,
 };
