0a1eae201b98ffc0de809b22e73eb95598f6bb9b
backmeupplz <backmeupplz@gmail.com>
2017-02-25 23:17:45 -0800

    add docs to public.js

--- routes/public.js
+++ routes/public.js
@@ -1,3 +1,4 @@
+/** Dependencies */
 const express = require('express');
 const dbmanager = require('../helpers/dbmanager');
 const errors = require('../helpers/errors');
@@ -6,6 +7,7 @@ const botReporter = require('../helpers/botReporter');
 
 const router = express.Router();
 
+/** Method to return reset password web page */
 router.get('/resetPassword', (req, res) => {
   const userId = req.query.userid;
   const token = req.query.token;
@@ -17,27 +19,24 @@ router.get('/resetPassword', (req, res) => {
     .then((user) => {
       if (!user) {
         res.render('error', { error: errors.noUserFound().message });
+      } else if (!user.tokenForPasswordResetIsFresh) {
+        res.render('error', { error: 'You can only use reset link once.' });
+      } else if (user.tokenForPasswordReset !== token) {
+        res.render('error', { error: 'You can only use reset link once.' });
       } else {
-        if (!user.tokenForPasswordResetIsFresh) {
-          res.render('error', { error: 'You can only use reset link once.' });
-        } else {
-          if (user.tokenForPasswordReset !== token) {
-            res.render('error', { error: 'You can only use reset link once.' });
-          } else {
-            user.tokenForPasswordResetIsFresh = false;
-            user.save()
-              .then(() => {
-                botReporter.reportGetResetPassword(user.email);
-                res.render('reset-password', { userid: userId, token: token });
-              })
-              .catch(err => res.render('error', { error: err.message || 'Something went wrong :(' }));
-          }
-        }
+        user.tokenForPasswordResetIsFresh = false;
+        user.save()
+          .then(() => {
+            botReporter.reportGetResetPassword(user.email);
+            res.render('reset-password', { userid: userId, token });
+          })
+          .catch(err => res.render('error', { error: err.message || 'Something went wrong :(' }));
       }
     })
     .catch(err => res.render('error', { error: err.message || 'Something went wrong :(' }));
 });
 
+/** Method to reset password */
 router.post('/resetPassword', (req, res) => {
   const password = String(req.body.password);
   const userId = req.body.userid;
@@ -52,28 +51,25 @@ router.post('/resetPassword', (req, res) => {
     .then((user) => {
       if (!user) {
         res.render('error', { error: errors.noUserFound().message });
+      } else if (user.tokenForPasswordReset !== token) {
+        res.render('error', { error: 'Wrong token provided.' });
       } else {
-        if (user.tokenForPasswordReset !== token) {
-          res.render('error', { error: 'Wrong token provided.' });
-        } else {
-          hash.hashPassword(password)
-            .then((result) => {
-              user.tokenForPasswordReset = null;
-              user.password = result;
-              user.save()
-                .then(() => {
-                  botReporter.reportResetPassword(user.email);
-                  res.render('success', { message: 'Password was updated!' });
-                })
-                .catch(err => res.render('error', { error: err.message || 'Something went wrong :(' }));
-            })
-            .catch(err => res.render('error', { error: err.message || 'Something went wrong :(' }));
-        }
+        hash.hashPassword(password)
+          .then((result) => {
+            user.tokenForPasswordReset = null;
+            user.password = result;
+            user.save()
+              .then(() => {
+                botReporter.reportResetPassword(user.email);
+                res.render('success', { message: 'Password was updated!' });
+              })
+              .catch(err => res.render('error', { error: err.message || 'Something went wrong :(' }));
+          })
+          .catch(err => res.render('error', { error: err.message || 'Something went wrong :(' }));
       }
     })
     .catch(err => res.render('error', { error: err.message || 'Something went wrong :(' }));
 });
 
-// Export
-
+/** Export */
 module.exports = router;
