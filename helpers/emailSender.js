5bd0134a89f3e8d36954787b3c9a30abdc38ee95
backmeupplz <backmeupplz@gmail.com>
2017-02-25 23:53:57 -0800

    add docs to emailSender.js

--- helpers/emailSender.js
+++ helpers/emailSender.js
@@ -1,15 +1,28 @@
+/**
+ * Module to send out emails
+ *
+ * @module emailSender
+ * @license MIT
+ */
+
+/** Dependencies */
 const helper = require('sendgrid').mail;
 const sg = require('sendgrid')('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');
 const hogan = require('hogan.js');
 const path = require('path');
 const fs = require('fs');
 
+/** Load email templates */
 const emailResetRawHtml = fs.readFileSync(path.join(__dirname, '../views/email-reset.hjs'), 'utf8');
 const emailResetPasswordTemplate = hogan.compile(emailResetRawHtml);
 
 const emailMagicRawHtml = fs.readFileSync(path.join(__dirname, '../views/email-magic-link.hjs'), 'utf8');
 const emailMagicLinkTemplate = hogan.compile(emailMagicRawHtml);
 
+/**
+ * Function to send reset password email
+ * @param {Mongo:User} user user that should get an email
+ */
 function sendResetPassword(user) {
   const fromEmail = new helper.Email('noreply@controlio.co');
   const toEmail = new helper.Email(user.email);
@@ -26,6 +39,10 @@ function sendResetPassword(user) {
   sg.API(request);
 }
 
+/**
+ * Function to send magic link email
+ * @param {Mongo:User} user user that should get an email
+ */
 function sendMagicLink(user) {
   const fromEmail = new helper.Email('noreply@controlio.co');
   const toEmail = new helper.Email(user.email);
@@ -42,6 +59,7 @@ function sendMagicLink(user) {
   sg.API(request);
 }
 
+/** Exports */
 module.exports = {
   sendResetPassword,
   sendMagicLink,
