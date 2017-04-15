219445a1f8a5b91f5f730de55d8b4c5ef7b17c82
backmeupplz <backmeupplz@gmail.com>
2017-04-14 23:31:42 -0700

    fix some texts

--- helpers/errors.js
+++ helpers/errors.js
@@ -153,6 +153,174 @@ function standardize(originalError) {
   return resultError;
 }
 
+/** Localization */
+const localizedList = {
+  NOT_FOUND_ERROR: {
+    en: 'Not found',
+    ru: 'Ресурс не найден',
+  },
+  NO_ACCESS_ERROR: {
+    en: 'No acess',
+    ru: 'Нет доступа',
+  },
+  EMAIL_NOT_REGISTERED_ERROR: {
+    en: 'Email not registered',
+    ru: 'Email не зарегистрирован',
+  },
+  INVALID_MANAGER_EMAIL_ERROR: {
+    en: 'Please provide a valid manager email',
+    ru: 'Пожалуйста, введите корректный email менеджера',
+  },
+  WRONG_PASSWORD_ERROR: {
+    en: 'Wrong password',
+    ru: 'Неверный пароль',
+  },
+  AUTH_TOKEN_FAILED_ERROR: {
+    en: 'Failed to authenticate token',
+    ru: 'Ошибка аутентификации токена',
+  },
+  USER_ALREADY_EXIST_ERROR: {
+    en: 'User already exists',
+    ru: 'Пользователь с таким email уже существует',
+  },
+  NO_API_KEY_ERROR: {
+    en: 'No API key provided',
+    ru: 'Ошибка API ключа',
+  },
+  USER_NOT_FOUND_ERROR: {
+    en: 'No user found',
+    ru: 'Пользователь не найден',
+  },
+  PASSWORD_ALREADY_EXIST_ERROR: {
+    en: 'You already have a password',
+    ru: 'У вас уже есть пароль',
+  },
+  PASSWORD_NOT_FOUND_ERROR: {
+    en: 'You haven\'t set password yet. We sent you an email to set it.',
+    ru: 'Пароль не установлен. Мы отправили вам email для создания пароля.',
+  },
+  OWNER_NOT_FOUND_ERROR: {
+    en: 'No owner found',
+    ru: 'Владелец не найден',
+  },
+  MANAGER_NOT_FOUND_ERROR: {
+    en: 'No manager found',
+    ru: 'Менеджер не найден',
+  },
+  CLIENT_OBJECT_NOT_CREATED_ERROR: {
+    en: 'No client objects created',
+    ru: 'Не получилось создать объект клиента',
+  },
+  ADD_SELF_AS_MANAGER_ERROR: {
+    en: 'You cannot add yourself as a manager',
+    ru: 'Вы не можете добавить себя в список менеджеров',
+  },
+  ADD_SELF_AS_CLIENT_ERROR: {
+    en: 'You cannot add yourself as a client',
+    ru: 'Вы не можете добавить себя в список клиентов',
+  },
+  USER_ALREADY_MANAGER_ERROR: {
+    en: 'This user is already a manager',
+    ru: 'Этот пользователь уже добавлен в список менеджеров',
+  },
+  PROJECT_NOT_FOUND_ERROR: {
+    en: 'No project found',
+    ru: 'Проект не найден',
+  },
+  POST_NOT_FOUND_ERROR: {
+    en: 'No post found',
+    ru: 'Пост не найден',
+  },
+  INVITE_NOT_FOUND_ERROR: {
+    en: 'No invite found',
+    ru: 'Приглашение не найдено',
+  },
+  INITIAL_STATUS_ERROR: {
+    en: 'Initial status should be less than 250 symbols',
+    ru: 'Статус не может превышать 250 символов',
+  },
+  NOT_AUTHORIZED_ERROR: {
+    en: 'Not authorized',
+    ru: 'Ошибка авторизации',
+  },
+  MAGIC_LINK_ALREADY_USED_ERROR: {
+    en: 'Magic link can be used only once',
+    ru: 'Магическая ссылка может быть использована только один раз',
+  },
+  DEMO_ERROR: {
+    en: 'You cannot do this as a demo account',
+    ru: 'Демо аккаунт так не умеет',
+  },
+  ADD_DEMO_AS_OWNER_ERROR: {
+    en: 'You cannot create project using a demo account',
+    ru: 'Вы не можете создавать проекты, используя демо аккаунт',
+  },
+  ADD_DEMO_AS_CLIENT_ERROR: {
+    en: 'You cannot add demo account as a client',
+    ru: 'Вы не можете добавить демо аккаунт в список клентов',
+  },
+  ADD_DEMO_AS_MANAGER_ERROR: {
+    en: 'You cannot add demo account as a manager',
+    ru: 'Вы не можете добавить демо аккаунт в список менеджеров',
+  },
+  REMOVE_YOURSELF_AS_MANAGER_ERROR: {
+    en: 'You cannot remove yourself as a manager',
+    ru: 'Вы не можете удалить себя из списка менеджеров',
+  },
+  NOT_YOUR_MANAGER_ERROR: {
+    en: 'This user is not your manager',
+    ru: 'Этот пользователь не является вашим менеджером',
+  },
+  FIELD_NOT_FOUND_ERROR: {
+    en: 'Required field not found',
+    ru: 'Нужно заполнить все необходимые поля',
+  },
+  LEAVE_AS_OWNER_ERROR: {
+    en: 'You cannot leave the project as an owner',
+    ru: 'Владелец не может покинуть проект',
+  },
+  LEAVE_AS_DEMO_ERROR: {
+    en: 'You cannot leave the project as a demo account',
+    ru: 'Вы не можете покидать проекты, используя демо аккаунт',
+  },
+  MANAGER_LIMIT_ERROR: {
+    en: 'This project has reached it\'s manager limits',
+    ru: 'Лимит менеджеров достигнут',
+  },
+  USER_LIMIT_ERROR: {
+    en: 'This project has reached it\'s users limits',
+    ru: 'Лимит клиентов достигнут',
+  },
+  FINISHED_ERROR: {
+    en: 'This project was finished.',
+    ru: 'Этот проект был завершен',
+  },
+  AUTH_PASS_RESET_TOKEN_FAILED: {
+    en: 'Failed to authenticate password reset token',
+    ru: 'Ошибка аутентификации токена сброса пароля',
+  },
+  NOT_ENOUGH_PROJECTS_ERROR: {
+    en: 'Please upgrade your plan in settings',
+    ru: 'Пожалуйста, увеличьте ваш план в настройках',
+  },
+  TOKEN_EXPIRED_ERROR: {
+    en: 'Token expired',
+    ru: 'Токен просрочился',
+  },
+  VALIDATION_ERROR: {
+    en: 'Validation error',
+    ru: 'Ошибка валидации',
+  },
+  DB_ERROR: {
+    en: 'Database error',
+    ru: 'Ошибка базы данных',
+  },
+  UNDECLARED_ERROR: {
+    en: 'Undeclared error',
+    ru: 'Неизвестная ошибка',
+  },
+};
+
 /** Exports */
 module.exports = {
   notFound,
@@ -193,4 +361,5 @@ module.exports = {
   projectIsFinished,
   wrongResetToken,
   standardize,
+  localizedList,
 };
