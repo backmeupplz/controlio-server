82a95cf6f380a61c44051e490f06196c6d90218e
Nikita Kolmogorov <backmeupplz@gmail.com>
2017-05-17 12:31:23 -0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -14,7 +14,7 @@ router.get('/error_list', (req, res) => {
 
 /** A list of features to enable in iOS app */
 router.get('/feature_list', (req, res) => {
-  res.send({ 0: false, 1: true });
+  res.send({ 0: true, 1: false });
 });
 
 const apple = {
