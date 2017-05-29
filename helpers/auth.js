c19966b179bdb2d96adbe82ed404ef46d9b3fb57
backmeupplz <backmeupplz@gmail.com>
2017-05-28 17:53:58 -0700

    wrapped auth.js with try\catch, add async\await and commented it

--- helpers/auth.js
+++ helpers/auth.js
@@ -11,46 +11,68 @@ const validate = require('express-validation');
 const validation = require('../validation/auth');
 
 /** Method to check if request is signed with a valid user token */
-function checkToken(req, res, next) {
-  validate(validation.token)(req, res, (err) => {
-    if (err) return next(err);
-
-    const token = req.get('token');
-    const { error, data } = jwt.verify(token);
-
-    if (error) {
-      return next(error);
-    }
-    if (!data) {
-      return next(errors.authTokenFailed());
-    }
-    db.findUserById(data.userid)
-      .select('+token')
-      .then((user) => {
-        if (!user) {
-          return next(errors.authEmailNotRegistered());
-        } else if (user.token !== token) {
-          return next(errors.authTokenFailed());
-        }
-        req.user = user;
-        next();
-      })
-      .catch(next);
-  });
+async function checkToken(req, res, next) {
+  try {
+    /** Use joi to validate token format */
+    validate(validation.token)(req, res, async (err) => {
+      /** Throw error if validation unsuccessful */
+      if (err) {
+        throw err;
+      }
+      /** Get req params */
+      const token = req.get('token');
+      /** Try to verify jwt token */
+      const { error, data } = jwt.verify(token);
+      /** Throw error if token is invalid */
+      if (error) {
+        throw error;
+      }
+      /** Throw error if token has no data in it */
+      if (!data) {
+        throw errors.authTokenFailed();
+      }
+      /** Get user from db */
+      const user = await db.findUserById(data.userid)
+        .select('+token');
+      /** Throw error if no users exists */
+      if (!user) {
+        throw errors.authEmailNotRegistered();
+      }
+      /** Throw error if jwt's don't match */
+      if (user.token !== token) {
+        throw errors.authTokenFailed();
+      }
+      /** Save user in req */
+      req.user = user;
+      /** Continue execution of router */
+      next();
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 /** Method to check if request is signed with a valid api key */
 function checkApiKey(req, res, next) {
-  validate(validation.apiKey)(req, res, (err) => {
-    if (err) return next(err);
-
-    const apiKey = req.get('apiKey');
-    if (apiKey === config.apiKey) {
+  try {
+    /** Use joi to validate api key format */
+    validate(validation.apiKey)(req, res, (err) => {
+      /** Throw error if validation unsuccessful */
+      if (err) {
+        throw err;
+      }
+      /** Get req params */
+      const apiKey = req.get('apiKey');
+      /** Throw error if api keys don't match */
+      if (apiKey !== config.apiKey) {
+        throw errors.noApiKey();
+      }
+      /** Continue execution of router */
       next();
-    } else {
-      next(errors.noApiKey());
-    }
-  });
+    });
+  } catch (err) {
+    next(err);
+  }
 }
 
 /** Exports */
