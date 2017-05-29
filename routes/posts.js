0913260997d6e2715111b9cc954c601e5f60620a
backmeupplz <backmeupplz@gmail.com>
2017-05-28 17:36:05 -0700

    make posts.js to use async  await

--- routes/posts.js
+++ routes/posts.js
@@ -12,54 +12,83 @@ const router = express.Router();
 router.use(auth.checkToken);
 
 /** Method to add new post to the database */
-router.post('/', validate(validation.post), demo.checkDemo, (req, res, next) => {
-  const userId = req.user._id;
-  const projectId = req.body.projectid;
-  const text = req.body.text;
-  const attachments = req.body.attachments;
-  const type = req.body.type || 'post';
-
-  db.addPost(userId, projectId, text, attachments, type)
-    .then((post) => {
-      res.send(post);
-    })
-    .catch(err => next(err));
+router.post('/',
+validate(validation.post),
+demo.checkDemo,
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const userId = req.user._id;
+    const projectId = req.body.projectid;
+    const text = req.body.text;
+    const attachments = req.body.attachments;
+    const type = req.body.type || 'post';
+    /** Add post */
+    const post = await db.addPost(userId, projectId, text, attachments, type);
+    /** Respond with post */
+    res.send(post);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Method to get a list of posts for the project */
-router.get('/', validate(validation.get), (req, res, next) => {
-  const userId = req.user._id;
-  const projectId = req.query.projectid;
-  const skip = parseInt(req.query.skip || 0, 10);
-  const limit = parseInt(req.query.limit || 20, 10);
-
-  db.getPosts(userId, projectId, skip, limit)
-    .then(posts => res.send(posts))
-    .catch(err => next(err));
+router.get('/',
+validate(validation.get),
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const userId = req.user._id;
+    const projectId = req.query.projectid;
+    const skip = parseInt(req.query.skip || 0, 10);
+    const limit = parseInt(req.query.limit || 20, 10);
+    /** Get posts */
+    const posts = await db.getPosts(userId, projectId, skip, limit);
+    /** Respond with posts */
+    res.send(posts);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Method to edit a post */
-router.put('/', validate(validation.put), demo.checkDemo, (req, res, next) => {
-  const userId = req.user._id;
-  const projectId = req.body.projectid;
-  const postId = req.body.postid;
-  const text = req.body.text;
-  const attachments = req.body.attachments;
-
-  db.editPost(userId, projectId, postId, text, attachments)
-    .then(post => res.send(post))
-    .catch(err => next(err));
+router.put('/',
+validate(validation.put),
+demo.checkDemo,
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const userId = req.user._id;
+    const projectId = req.body.projectid;
+    const postId = req.body.postid;
+    const text = req.body.text;
+    const attachments = req.body.attachments;
+    /** Edit post */
+    const post = await db.editPost(userId, projectId, postId, text, attachments);
+    /** Respond with resulting post */
+    res.send(post);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Method to delete the post */
-router.delete('/', validate(validation.delete), demo.checkDemo, (req, res, next) => {
-  const userId = req.user._id;
-  const projectId = req.body.projectid;
-  const postId = req.body.postid;
-
-  db.deletePost(userId, projectId, postId)
-    .then(() => res.send({ success: true }))
-    .catch(err => next(err));
+router.delete('/',
+validate(validation.delete),
+demo.checkDemo,
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const userId = req.user._id;
+    const projectId = req.body.projectid;
+    const postId = req.body.postid;
+    /** Delete post */
+    await db.deletePost(userId, projectId, postId);
+    /** Respond with success */
+    res.send({ success: true });
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Export */
