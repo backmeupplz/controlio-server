1ba49d6fb7dc61940b384e42923fe1fa4f2e69f8
backmeupplz <backmeupplz@gmail.com>
2017-05-28 17:43:42 -0700

    change payments.js to async await

--- routes/payments.js
+++ routes/payments.js
@@ -13,66 +13,111 @@ const router = express.Router();
 router.use(auth.checkToken);
 
 /** Proxy method to stripe servers to get customer object */
-router.get('/customer', validate(validation.customer), (req, res, next) => {
-  const customerid = req.query.customerid;
-  payments.getStripeCustomer(customerid)
-    .then(customer => res.send(customer))
-    .catch(err => next(err));
+router.get('/customer',
+validate(validation.customer),
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const customerid = req.query.customerid;
+    /** Get stripe customer */
+    const customer = await payments.getStripeCustomer(customerid);
+    /** Respond with customer */
+    res.send(customer);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Check if not demo */
 router.use(demo.checkDemo);
 
 /** Proxy method to stripe servers to add payment sources to the customer */
-router.post('/customer/sources', validate(validation.postSource), (req, res, next) => {
-  const customerid = req.body.customerid;
-  const source = req.body.source;
-  payments.addStripeSource(customerid, source)
-    .then(newSource => res.send(newSource))
-    .catch(err => next(err));
+router.post('/customer/sources',
+validate(validation.postSource),
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const customerid = req.body.customerid;
+    const source = req.body.source;
+    /** Add stripe source */
+    const newSource = await payments.addStripeSource(customerid, source);
+    /** Respond with the new source */
+    res.send(newSource);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 /** Proxy method to stripe servers to change the default payment source of the customer */
-router.post('/customer/default_source', validate(validation.defaultSource), (req, res, next) => {
-  const customerid = req.body.customerid;
-  const source = req.body.source;
-  payments.setStripeDefaultSource(customerid, source)
-    .then(customer => res.send(customer))
-    .catch(err => next(err));
+router.post('/customer/default_source',
+validate(validation.defaultSource),
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const customerid = req.body.customerid;
+    const source = req.body.source;
+    /** Set default source */
+    const customer = await payments.setStripeDefaultSource(customerid, source);
+    /** Respond with customer */
+    res.send(customer);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Method to change customer's subscription at Controlio */
-router.post('/customer/subscription', validate(validation.subscription), (req, res, next) => {
-  const planid = req.body.planid;
-  const userId = req.user._id;
-  db.setSripeSubscription(userId, planid)
-    .then(user => res.send(user))
-    .catch(err => next(err));
+router.post('/customer/subscription',
+validate(validation.subscription),
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const planid = req.body.planid;
+    const userId = req.user._id;
+    /** Set subscription */
+    const user = await db.setSripeSubscription(userId, planid);
+    /** Respond with user */
+    res.send(user);
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Method to apply coupon (usually discount) at Controlio */
-router.post('/customer/coupon', validate(validation.coupon), (req, res, next) => {
-  const coupon = req.body.coupon.toLowerCase();
-  const userId = req.user._id;
-  db.applyStripeCoupon(userId, coupon)
-    .then(() => {
-      if (coupon.toLowerCase() === 'friend') {
-        return db.decrementNumberOfFriend();
-      }
-    })
-    .then(() => {
-      res.send({ success: true });
-    })
-    .catch(err => next(err));
+router.post('/customer/coupon',
+validate(validation.coupon),
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const coupon = req.body.coupon.toLowerCase();
+    const userId = req.user._id;
+    /** Apply stripe coupon */
+    await db.applyStripeCoupon(userId, coupon);
+    /** If coupon is 'friend', decrement number of such used coupons */
+    if (coupon.toLowerCase() === 'friend') {
+      await db.decrementNumberOfFriend();
+    }
+    /** Respond with success */
+    res.send({ success: true });
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Proxy method to stripe servers to delete a card */
-router.delete('/customer/card', validate(validation.deleteCard), (req, res, next) => {
-  const customerid = req.body.customerid;
-  const cardid = req.body.cardid;
-  payments.deleteCard(customerid, cardid)
-    .then(() => res.send({ success: true }))
-    .catch(err => next(err));
+router.delete('/customer/card',
+validate(validation.deleteCard),
+async (req, res, next) => {
+  try {
+    /** Get req params */
+    const customerid = req.body.customerid;
+    const cardid = req.body.cardid;
+    /** Delete card */
+    await payments.deleteCard(customerid, cardid);
+    /** Respond with success */
+    res.send({ success: true });
+  } catch (err) {
+    next(err);
+  }
 });
 
 /** Export */
