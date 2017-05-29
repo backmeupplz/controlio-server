c2c78dcf695f77cd8e95f1499db7e6fa13f5e588
backmeupplz <backmeupplz@gmail.com>
2017-05-28 18:10:03 -0700

    fix jwt.js docs

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,6 +1,11 @@
 const jwt = require('jsonwebtoken');
 const secret = require('../config').jwtSecret;
 
+/**
+ * Method to verify jwt token format
+ * @param {String} token Token to verify
+ * @return {data, error} Object containing either error or decrypted data
+ */
 function verify(token) {
   const result = {};
   try {
@@ -11,6 +16,12 @@ function verify(token) {
   return result;
 }
 
+/**
+ * Method to create jwt token
+ * @param {Object} payload Object that gets saved to jwt token
+ * @param {Object} options Various options
+ * @return {Promise(String)} Promise with resulting token
+ */
 function sign(payload, options) {
   return jwt.sign(payload, secret, options);
 }
